981904e0df5f93a689e821ad1382bfa39a2ca8c8
Julio <jc.gutierrezq@gmail.com>
2020-05-16 18:28:19 -0500

    static files

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
+
 
 const App = () => (
     <div className="App">
         <Header />
         <Search />
-
+        <Categories>
+            <Carousel>
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+        <Footer />
     </div>
 );
 
